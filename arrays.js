d20c952682ed0f21fe6ec908c086ca2bb636c2eb
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-28 20:15:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,18 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element) {
-  return [element, ...array]
+  return [...array ,element]
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element);
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
   return array
